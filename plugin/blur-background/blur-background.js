3901106a6dceba849e2308a212248637844c6b3c
Evgeny Kot <bunopus@gmail.com>
2021-09-15 14:58:23 +0200

    moar

--- plugin/blur-background/blur-background.js
+++ plugin/blur-background/blur-background.js
@@ -8,7 +8,8 @@
     Reveal.addEventListener('slidechanged', function (slide) {
         if (slide.currentSlide.hasAttribute('data-background-blur')) {
             setTimeout(() => {
-                let background = $('.reveal div.backgrounds .present:visible:not(.stack)');
+                let bg_image = slide.currentSlide.getAttribute('data-background-image');
+                let background = $(`.reveal div.backgrounds .present:visible:not(.stack)[data-background-hash*='${bg_image}']`);
                 background.css({ filter: `blur(20px) opacity(50%)` });
             }, 100);
         }
